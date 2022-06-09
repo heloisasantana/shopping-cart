dae12de97761c458d1f75bbfb03a6cd570bc3eb1
heloisasantana <heloisasantana.silva@hotmail.com>
2022-06-09 11:45:01 -0300

    Conclusão do Requisito 9

--- script.js
+++ script.js
@@ -12,17 +12,27 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const fatherCart = document.querySelector('.cart__items');
+const fatherCart = document.querySelector('.cart');
+const fatherCartItems = document.querySelector('.cart__items');
+
+let sum = 0; 
+const sumItemsCart = createCustomElement('h4', 'total-price', sum);
+fatherCart.appendChild(sumItemsCart);
 
 const cartItemClickListener = (event) => {
-  fatherCart.removeChild(event.target);
+  fatherCartItems.removeChild(event.target);
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // Referência utilizada para inserir Math.round(number * 100)/100 para obter duas casas decimais e não utilizar o toFixed, conforme solicitado pelo requisito 9: https://metring.com.br/arredondar-numero-em-javascript;
+  sumItemsCart.innerText = (Math.round(sum += salePrice * 100) / 100);
   li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', () => {
+  sumItemsCart.innerText = (Math.round(sum -= salePrice * 100) / 100);
+  });
   return li;
 };
 
@@ -36,7 +46,7 @@ const createProductItemElement = ({ sku, name, image }) => {
   .addEventListener('click', async () => {
     const pickedItem = await fetchItem(sku);
     const objItem = { sku: pickedItem.id, name: pickedItem.title, salePrice: pickedItem.price };
-    fatherCart.appendChild(createCartItemElement(objItem));
+    fatherCartItems.appendChild(createCartItemElement(objItem));
   });
   return section;
 };
